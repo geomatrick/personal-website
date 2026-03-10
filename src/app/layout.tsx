7e1e634553e68a1a128baccec3c0997af5e1cef7
patrickwoodhead <patrick.woodhead@protocol.ai>
2026-03-10 11:57:01 -0400

    ga tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Benne } from "next/font/google";
 import "./globals.css";
 
@@ -21,6 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TL2KT9EXMN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TL2KT9EXMN');
+          `}
+        </Script>
+      </head>
       <body
         className={`${benne.className} antialiased`}
       >
